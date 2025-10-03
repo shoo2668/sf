5dba4c2fb43695d18d2804a05cff0358e7b357a1
shoo2668 <shoo2668@gmail.com>
2025-10-03 15:00:02 +0900

    1.6.25

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'delivery-sheet-cache-v1.6.24-update'; // 버전을 명확하게 올립니다.
+const CACHE_NAME = 'delivery-sheet-cache-v1.6.25-update'; // 버전을 명확하게 올립니다.
 const urlsToCache = [
   '/sf/',
   '/sf/index.html',
